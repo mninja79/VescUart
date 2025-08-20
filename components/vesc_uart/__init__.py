d523091ed4143dec8110aa00cd8752adb91944e9
mninja79 <imusilu@hotmail.com>
2025-08-20 23:52:49 +0200

    still testing

--- components/vesc_uart/__init__.py
+++ components/vesc_uart/__init__.py
@@ -1,13 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
-from esphome.const import CONF_ID, CONF_UPDATE_INTERVAL
+from esphome.components import polling_component
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ["uart"]
 
 vesc_uart_ns = cg.esphome_ns.namespace("vesc_uart")
-VescUART = vesc_uart_ns.class_("VescUART", cg.PollingComponent, uart.UARTDevice)
-VescData = vesc_uart_ns.struct("VescData")
+VescUART = vesc_uart_ns.class_("VescUART", polling_component.PollingComponent, uart.UARTDevice)
 
 CONFIG_SCHEMA = (
     cv.Schema(
@@ -15,10 +15,9 @@ CONFIG_SCHEMA = (
             cv.GenerateID(): cv.declare_id(VescUART),
         }
     )
-    .extend(cv.PollingComponent.polling_schema(CONF_UPDATE_INTERVAL))
+    .extend(polling_component.polling_component_schema(2000))
     .extend(uart.UART_DEVICE_SCHEMA)
 )
-
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
