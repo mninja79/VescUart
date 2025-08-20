ae742b98c9c0d0f08ecab555c45c5ccc309687a5
mninja79 <imusilu@hotmail.com>
2025-08-21 01:42:34 +0200

    part2

--- components/vesc_uart/vesc_uart_component.cpp
+++ components/vesc_uart/vesc_uart_component.cpp
@@ -1,36 +1,100 @@
-#include "vesc_uart_component.h"
+// VescUartComponent.cpp
+
+#include "VescUartComponent.h"
 #include "esphome/core/log.h"
-#include "VescUart.h"
 
 namespace esphome {
 namespace vesc_uart {
 
-static const char *TAG = "vesc_uart";
+VescUartComponent::VescUartComponent() : VescUartComponent(1000) {}
 
-void VescUART::setup() {
-  this->set_uart_device(this);
-  ESP_LOGCONFIG(TAG, "VESC UART Component is being set up.");
+VescUartComponent::VescUartComponent(uint32_t timeout_ms)
+    : PollingComponent(1000) {
+    this->timeout_ms_ = timeout_ms;
 }
 
-void VescUART::update() {
-    if (this->available()) {
-        int success = vesc_uart_.getVescValues();
-        if (success == 1) {
-            this->data.avgMotorCurrent = vesc_uart_.data.avgMotorCurrent;
-            this->data.avgInputVoltage = vesc_uart_.data.avgInputVoltage;
-            this->data.rpm = vesc_uart_.data.rpm;
-            ESP_LOGD(TAG, "VESC Data: Current=%.2fA, Voltage=%.2fV, RPM=%.0f", this->data.avgMotorCurrent, this->data.avgInputVoltage, this->data.rpm);
-        } else {
-            ESP_LOGW(TAG, "Failed to read VESC values. Error code: %d", success);
-        }
+VescUartComponent::VescUartComponent(sensor::Sensor *voltage_sensor, sensor::Sensor *current_sensor, sensor::Sensor *power_sensor,
+                                     sensor::Sensor *rpm_sensor, sensor::Sensor *temp_sensor, sensor::Sensor *ah_consumed_sensor,
+                                     sensor::Sensor *ah_charged_sensor, sensor::Sensor *watt_hours_consumed_sensor,
+                                     sensor::Sensor *watt_hours_charged_sensor, sensor::Sensor *status_sensor, uint32_t timeout_ms)
+    : PollingComponent(1000) {
+    this->voltage_sensor_ = voltage_sensor;
+    this->current_sensor_ = current_sensor;
+    this->power_sensor_ = power_sensor;
+    this->rpm_sensor_ = rpm_sensor;
+    this->temp_sensor_ = temp_sensor;
+    this->ah_consumed_sensor_ = ah_consumed_sensor;
+    this->ah_charged_sensor_ = ah_charged_sensor;
+    this->watt_hours_consumed_sensor_ = watt_hours_consumed_sensor;
+    this->watt_hours_charged_sensor_ = watt_hours_charged_sensor;
+    this->status_sensor_ = status_sensor;
+    this->timeout_ms_ = timeout_ms;
+}
+  
+void VescUartComponent::setup() {
+  // This function runs once on startup.
+  // Initialize the VESC UART library and set up the serial port.
+  vesc_uart_.setSerialPort(this);
+  vesc_uart_.set_timeout(this->timeout_ms_);
+  ESP_LOGCONFIG(TAG, "VESC UART Component initialized with timeout: %u ms", this->timeout_ms_);
+  
+  // Set an initial value for the status sensor
+  if (this->status_sensor_ != nullptr) {
+      this->status_sensor_->publish_state(0); // 0 = Not connected
+  }
+}
+
+void VescUartComponent::update() {
+  // This function is called at a regular interval defined by update_interval.
+  // It is a more robust way to poll for data than using a non-blocking loop().
+  
+  ESP_LOGD(TAG, "Requesting VESC telemetry data...");
+  
+  // Attempt to get VESC values. The VescUart library handles the request/response.
+  if (vesc_uart_.getVescValues()) {
+    // Data was successfully received from the VESC.
+    ESP_LOGD(TAG, "VESC telemetry received!");
+
+    // Publish sensor values. Check if each sensor is configured before publishing.
+    if (this->voltage_sensor_ != nullptr) {
+      this->voltage_sensor_->publish_state(vesc_uart_.data.inpVoltage);
     }
+    if (this->current_sensor_ != nullptr) {
+      this->current_sensor_->publish_state(vesc_uart_.data.avgInputCurrent);
+    }
+    if (this->power_sensor_ != nullptr) {
+      this->power_sensor_->publish_state(vesc_uart_.data.inpVoltage * vesc_uart_.data.avgInputCurrent);
+    }
+    if (this->rpm_sensor_ != nullptr) {
+      this->rpm_sensor_->publish_state(vesc_uart_.data.rpm);
+    }
+    if (this->temp_sensor_ != nullptr) {
+      this->temp_sensor_->publish_state(vesc_uart_.data.tempFET + vesc_uart_.data.tempMotor / 2); // Simple average
+    }
+    if (this->ah_consumed_sensor_ != nullptr) {
+      this->ah_consumed_sensor_->publish_state(vesc_uart_.data.ampHours);
+    }
+    if (this->ah_charged_sensor_ != nullptr) {
+      this->ah_charged_sensor_->publish_state(vesc_uart_.data.ampHoursCharged);
+    }
+    if (this->watt_hours_consumed_sensor_ != nullptr) {
+      this->watt_hours_consumed_sensor_->publish_state(vesc_uart_.data.wattHours);
+    }
+    if (this->watt_hours_charged_sensor_ != nullptr) {
+      this->watt_hours_charged_sensor_->publish_state(vesc_uart_.data.wattHoursCharged);
+    }
+    // Update status sensor to show successful connection.
+    if (this->status_sensor_ != nullptr) {
+      this->status_sensor_->publish_state(1); // 1 = Connected
+    }
+  } else {
+    // Failed to get data. This could be due to a timeout or other serial error.
+    ESP_LOGW(TAG, "Failed to read VESC telemetry. Check connection and power.");
+    if (this->status_sensor_ != nullptr) {
+      this->status_sensor_->publish_state(0); // 0 = Not connected
+    }
+  }
 }
 
-void VescUART::dump_config() {
-  ESP_LOGCONFIG(TAG, "VESC UART:");
-  LOG_UART_DEVICE(this);
-  ESP_LOGCONFIG(TAG, "  Update Interval: %dms", this->get_update_interval());
-}
-
-} // namespace vesc_uart
-} // namespace esphome
+}  // namespace vesc_uart
+}  // namespace esphome
--- components/vesc_uart/vesc_uart_component.h
+++ components/vesc_uart/vesc_uart_component.h
@@ -1,29 +1,50 @@
-#pragma once
+// VescUartComponent.h
+
+#ifndef VESC_UART_COMPONENT_H
+#define VESC_UART_COMPONENT_H
 
 #include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
 #include "esphome/components/uart/uart.h"
 #include "VescUart.h"
 
 namespace esphome {
 namespace vesc_uart {
 
-struct VescData {
-    float avgMotorCurrent;
-    float avgInputVoltage;
-    float rpm;
-};
+static const char *TAG = "vesc_uart";
 
-class VescUART : public PollingComponent, public uart::UARTDevice {
+class VescUartComponent : public PollingComponent, public uart::UARTDevice {
  public:
+  // Default constructor with a timeout of 1000ms.
+  VescUartComponent();
+  // Constructor with a configurable timeout.
+  VescUartComponent(uint32_t timeout_ms);
+  // Full constructor for all sensors and timeout.
+  VescUartComponent(sensor::Sensor *voltage_sensor, sensor::Sensor *current_sensor, sensor::Sensor *power_sensor,
+                    sensor::Sensor *rpm_sensor, sensor::Sensor *temp_sensor, sensor::Sensor *ah_consumed_sensor,
+                    sensor::Sensor *ah_charged_sensor, sensor::Sensor *watt_hours_consumed_sensor,
+                    sensor::Sensor *watt_hours_charged_sensor, sensor::Sensor *status_sensor, uint32_t timeout_ms = 1000);
+
+  // ESPHome component lifecycle methods.
   void setup() override;
   void update() override;
-  void dump_config() override;
-
-  VescData data;
 
  protected:
   VescUart vesc_uart_;
+  uint32_t timeout_ms_;
+  sensor::Sensor *voltage_sensor_{nullptr};
+  sensor::Sensor *current_sensor_{nullptr};
+  sensor::Sensor *power_sensor_{nullptr};
+  sensor::Sensor *rpm_sensor_{nullptr};
+  sensor::Sensor *temp_sensor_{nullptr};
+  sensor::Sensor *ah_consumed_sensor_{nullptr};
+  sensor::Sensor *ah_charged_sensor_{nullptr};
+  sensor::Sensor *watt_hours_consumed_sensor_{nullptr};
+  sensor::Sensor *watt_hours_charged_sensor_{nullptr};
+  sensor::Sensor *status_sensor_{nullptr};
 };
 
-} // namespace vesc_uart
-} // namespace esphome
+}  // namespace vesc_uart
+}  // namespace esphome
+
+#endif  // VESC_UART_COMPONENT_H
